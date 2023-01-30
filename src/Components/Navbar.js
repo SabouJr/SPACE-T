8b779bf70c48359ce3d2038a4b6bdec1f0369bf9
PauloWebD <paulmonhurel95@gmail.com>
2023-01-30 23:29:43 +0100

    test

---
+++ src/Components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../styles/Navbar.css'
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        SPACE-T
+      </a>
+      <div className="navigation-menu">
+        <ul>
+          <li>
+            <NavLink to={'/'}></NavLink>
+          </li>
+          <li>
+            <NavLink to={'/Accueil'}> Accueil</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/coming'}> Accueilll</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
